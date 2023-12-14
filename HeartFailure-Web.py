3645a1d9fe6a1c78563b718ca831aaae6b53cd27
glorycornelia <glorycornelia26@gmail.com>
2023-12-14 09:06:50 +0700

    design added

--- HeartFailure-Web.py
+++ HeartFailure-Web.py
@@ -9,9 +9,10 @@ def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
 
+st.set_page_config(layout="wide")
+
 # Load lottie file
 loaded_lottie = load_lottiefile('Animation/heart_animation.json')
-st_lottie(loaded_lottie, speed=0.5,reverse=False,loop=True,quality="low",height=200,width=200)
 
 # loading the saved models
 heart_failure_model = pickle.load(open('gnb_model.sav','rb'))
@@ -24,49 +25,32 @@ with st.sidebar:
     selected = option_menu('Prediction System',
                           
                           ['Heart Failure Prediction'],
-                          icons=['heart'],
                           default_index=0)
     
 
 # Heart Failure Prediction Page
 if (selected == 'Heart Failure Prediction'):
-    
-    # page title
-    st.title('Heart Failure Prediction using Naive Bayes')
-    
+
     col1, col2 = st.columns(2)
     
     with col1:
+        # page title
+        st.title('Heart Failure Prediction using Naive Bayes')
+        st.write('Web aplication that implements Naive Bayes method to predict Heart Failure Desease. Please fill the form on the right and start your prediction!')
+        st.markdown("---")
+        st_lottie(loaded_lottie, speed=0.5,reverse=False,loop=True,quality="low",height=500,width=500)
+
+    with col2:
         age = st.text_input('Age')
-        
-    with col2:
         sex = st.text_input('Sex')
-        
-    with col1:
         cp = st.text_input('Chest Pain types')
-        
-    with col2:
         trestbps = st.text_input('Resting Blood Pressure')
-        
-    with col1:
         chol = st.text_input('Serum Cholestoral in mg/dl')
-        
-    with col2:
         fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
-        
-    with col1:
         restecg = st.text_input('Resting Electrocardiographic results')
-        
-    with col2:
         thalach = st.text_input('Maximum Heart Rate achieved')
-        
-    with col1:
         exang = st.text_input('Exercise Induced Angina')
-        
-    with col2:
         oldpeak = st.text_input('ST depression induced by exercise')
-        
-    with col1:
         slope = st.text_input('Slope of the peak exercise ST segment')
      
      
@@ -83,11 +67,9 @@ if (selected == 'Heart Failure Prediction'):
                                            'Oldpeak', 'ST_Slope'])
 
         input_data[['Sex','ChestPainType','RestingECG','ExerciseAngina','ST_Slope']]=oe.transform(input_data[['Sex','ChestPainType','RestingECG','ExerciseAngina','ST_Slope']])
-        print(input_data)
 
         input_data=scaler.transform(input_data)
-        print(input_data)
-
+        
         heart_prediction = heart_failure_model.predict(input_data)                          
 
         if (heart_prediction[0] == 1):
